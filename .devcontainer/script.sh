9c66f608c6235077d129376c2c20b437579eddce
Anika Gupta <anikagupta0316@gmail.com>
2024-12-23 11:04:02 +0000

    devcontainer

---
+++ .devcontainer/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -n "$GOOGLE_APPLICATION_CREDENTIALS_BASE64" ]; then
+    echo "@zero65:registry=https://asia-south1-npm.pkg.dev/zero65/npm/" > ~/.npmrc
+    mkdir -p ~/.config/gcloud
+    echo "$GOOGLE_APPLICATION_CREDENTIALS_BASE64" | base64 --decode > ~/.config/gcloud/application_default_credentials.json
+    npm install -g google-artifactregistry-auth
+    npx google-artifactregistry-auth
+fi
+
+npm update && npm run dev
