c1e3e8506a1b20223df0d15a82e1c7e3db9705c3
Anika Gupta <anikagupta0316@gmail.com>
2024-12-24 05:36:39 +0000

    duck storage

---
+++ src/index.js
@@ -0,0 +1,61 @@
+const { DuckDBInstance } = require('@duckdb/node-api');
+
+class DuckStorage {
+
+  #connection = null;
+  #tableLocks = {};
+
+  constructor() {
+    DuckDBInstance.create(':memory:')
+        .then(instance => instance.connect())
+        .then(connection => this.#connection = connection)
+        .catch(error => console.error('Error:', error))
+  }
+
+  #waitForDBConnection = async () => {
+    while(!this.#connection) {
+      await new Promise(resolve => setTimeout(resolve, 10));
+      console.log('Waiting for the connection to be ready ...')
+    }
+  }
+
+  #checkTableExists = async (schema, table) => {
+
+    const query = `
+        SELECT COUNT(*) 
+        FROM information_schema.tables 
+        WHERE table_name = '${table}' AND table_schema = '${schema}';`;
+
+    const result = await this.#connection.run(query);
+    const rows = await result.getRows();
+    const count = rows[0][0];
+
+    return count > 0;
+
+  }
+
+  #loadTableFromGCS = async (tableName) => {
+    // stream data from gcs and load to table
+  }
+
+  exec = async (schema, table, query) => {
+
+    await this.#waitForDBConnection();
+
+    while(this.#tableLocks[`${schema}.${table}`])
+      await new Promise(resolve => setTimeout(resolve, 10));
+
+    this.#tableLocks[`${schema}.${table}`] = true;
+
+    if(!await this.#checkTableExists(schema, table))
+      await this.#loadTableFromGCS(schema, table);
+
+    this.#tableLocks[`${schema}.${table}`] = false;
+
+    return this.#connection.run(query.replace('{table}', `${schema}.${table}`));
+
+  }
+
+}
+
+module.exports = DuckStorage;
