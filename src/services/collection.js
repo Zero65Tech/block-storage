9509243c6d382a0ad2e7ea83639489534971ec22
Anika Gupta <anikagupta0316@gmail.com>
2024-12-25 15:09:16 +0000

    persisting data to gcs

--- src/services/collection.js
+++ src/services/collection.js
@@ -1,11 +1,15 @@
 const readline = require('readline');
 const bucket = require('../config/storage.js');
 
+const Log = new (require('@zero65tech/log'));
+
 class CollectionService {
 
   #collectionPath;
   #collectionName;
   #collection;
+  #collectionLastUpdated = new Date();
+  #collectionLastPersisted = new Date();
 
   constructor(collectionPath, collectionName) {
     this.#collectionPath = collectionPath;
@@ -31,8 +35,8 @@ class CollectionService {
       });
 
       rl.on('line', (line) => {
-        const { key, value, timestamp } = JSON.parse(line);
-        map.set(key, { value, timestamp });
+        const { key, timestamp, data } = JSON.parse(line);
+        map.set(key, { data, timestamp });
       });
 
       rl.on('error', (e) => {
@@ -40,7 +44,7 @@ class CollectionService {
           this.#collection = map;
           resolve();
         } else {
-          this.#collection = null;
+          this.#collection = undefined;
           reject(e);
         }
       });
@@ -58,24 +62,57 @@ class CollectionService {
     await this.#init();
     this.get = (key) => {
       const entry = this.#collection.get(key);
-      return entry ? entry.value : null;
+      return entry ? entry.data : null;
     }
     return this.get(key);
   }
 
-  async set(key, value) {
+  async set(key, data) {
     await this.#init();
-    this.set = (key, value) => {
-      this.#collection.set(key, { value, timestamp: new Date() });
+    this.set = (key, data) => {
+      this.#collection.set(key, { data, timestamp: new Date() });
+      this.#collectionLastUpdated = new Date();
     }
-    return this.set(key, value);
+    return this.set(key, data);
   }
 
   async persist() {
     await this.#init();
     this.persist = async () => {
-      // TODO write to GCS
+
+      if(!this.#collectionLastUpdated <= this.#collectionLastPersisted)
+        return;
+
+      const dateRef = this.#collectionLastUpdated;
+
+      await new Promise((resolve, reject) => {
+
+        let ws = bucket.file(this.#collectionPath + this.#collectionName).createWriteStream();
+
+        ws.on('error', (e) => {
+          reject(e);
+        });
+
+        ws.on('finish', () => {
+          this.#collectionLastPersisted = dateRef;
+          Log.notice(`${ this.#collectionPath + this.#collectionName } persisted to GCS !`);
+          resolve();
+        });
+
+        const keys = Array.from(this.#collection.keys());
+        keys.sort();
+
+        for(const key of keys) {
+          const { data, timestamp } = this.#collection.get(key);
+          ws.write(JSON.stringify({ key, timestamp, data }) + '\n');
+        }
+
+        ws.end();
+
+      });
+
     }
+    await this.persist();
   }
 
 }
