edbbde7e9365a07d9ad11eade57fe04455e12ae1
Anika Gupta <anikagupta0316@gmail.com>
2024-12-25 03:27:14 +0000

    app.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 process.env.STAGE = process.env.STAGE || 'alpha';
 process.env.PORT  = process.env.PORT  || 8080;
 
-require('./legacy').listen(
+require('./app').listen(
     process.env.PORT,
-    console.log(`Server (${ process.env.STAGE }) is up and listening at ${ process.env.PORT } port.`));
+    console.log(`Server (${process.env.STAGE}) is up and listening at ${process.env.PORT} port.`));
