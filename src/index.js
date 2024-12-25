dbc2002bee160a912527eda0bd785437a29578f4
Anika Gupta <anikagupta0316@gmail.com>
2024-12-25 09:26:43 +0000

    SIGTERM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 process.env.STAGE = process.env.STAGE || 'alpha';
 process.env.PORT  = process.env.PORT  || 8080;
 
-require('./app').listen(
-    process.env.PORT,
-    console.log(`Server (${process.env.STAGE}) is up and listening at ${process.env.PORT} port.`));
+const app = require('./app');
+const server = app.listen(process.env.PORT, console.log(`Server (${process.env.STAGE}) is up and listening at ${process.env.PORT} port.`));
+
+process.on('SIGTERM', () => {
+
+  console.log('SIGTERM received. Closing server...');
+
+  server.close(() => {
+    console.log('Server closed.');
+    process.exit(0);
+  });
+
+});
